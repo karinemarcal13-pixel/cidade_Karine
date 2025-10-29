788ba32d0a163978e6ddf760bdc186288e1f0db4
karinemarcal13-pixel <karinemarcal13@gmail.com>
2025-10-29 20:51:09 -0300

    bb

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.getElementById('form-contato').addEventListener('submit', function(e) {
     e.preventDefault();
-
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
-    const mensagem = document.getElementById('mensagem').value;
+    const msg = document.getElementById('mensagem').value;
     const resposta = document.getElementById('resposta');
 
-    if (nome && email && mensagem) {
-        resposta.textContent = `Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`;
-        resposta.style.color = 'green';
+    if (nome && email && msg) {
+        resposta.textContent = `Obrigada, ${nome}! Sua mensagem foi enviada com sucesso 💌`;
+        resposta.style.color = "green";
         this.reset();
     } else {
-        resposta.textContent = 'Por favor, preencha todos os campos.';
-        resposta.style.color = 'red';
-    } 
+        resposta.textContent = "Por favor, preencha todos os campos!";
+        resposta.style.color = "red";
+    }
 });
